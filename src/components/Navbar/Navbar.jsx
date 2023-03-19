b5837ec42b687bbc269c499a3bf9c735df5598b2
sametcoh <sametcoh@gmail.com>
2023-03-19 17:30:57 +0200

    added outside click of nabar to close

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,12 +9,27 @@ import NavbarSharedStyles from "./NavbarShared.module.css";
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
+  document.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (navbarOpen && event.target.ariaHasPopup !== "menu") {
+      setNavbarOpen(false)
+    }
+  });
+
+  const preventRendering = (event) => {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
+
   return (
     <nav>
-      <div className={`${NavbarSharedStyles.bars_menu} ${navbarOpen ? NavbarSharedStyles.active : ""}`} onClick={() => setNavbarOpen((prev) => !prev)}> <Icons.FaBars /> </div>
+      <div className={`${NavbarSharedStyles.bars_menu} ${navbarOpen ? NavbarSharedStyles.active : ""}`} onClick={(event) => { setNavbarOpen((prev) => !prev); preventRendering(event) }}> <Icons.FaBars /> </div>
       <ul className={`${NavbarSharedStyles.navbar_container} ${navbarOpen ? NavbarSharedStyles.active : NavbarSharedStyles.disabled}`}>
         {navbarItemsList.map((navItem, index) => {
-          return <NavbarItems items={navItem} key={index} close_menu={()=>setNavbarOpen(false)}/>;
+          return <NavbarItems items={navItem} key={index} close_menu={(event) => { setNavbarOpen(false); preventRendering(event) }} />;
         })}
       </ul>
     </nav>
